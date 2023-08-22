fa76ee3d52ed38b2515d908376aec944db3fc9ec
Mary.rani <mary.rani@stackroute.in>
2023-08-22 21:21:35 +0530

    first

---
+++ src/main/java/com/stackroute/stackapp/StackappApplication.java
@@ -0,0 +1,14 @@
+package com.stackroute.stackapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackappApplication {
+
+	public static void main(String[] args) {
+		System.out.println("welcome ");
+		SpringApplication.run(StackappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stackroute/stackapp/StackappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackroute.stackapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
